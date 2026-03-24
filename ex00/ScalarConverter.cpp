13439551636dc4c6b944e7d1797718b341f0303e
jchiu <jchiu@f1br8s7.paris.42.school>
2026-03-24 13:19:21 +0100

    a finir

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -19,38 +19,81 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-void ScalarConverter::convert(std::string input) 
-{
-    double d = std::atof(input.c_str());
+// void ScalarConverter::convert(std::string input) 
+// {
+//     double d = std::atof(input.c_str());
 
-    std::cout << "char : ";
-    // if (input.length() == 1 && !isdigit(d))
-    //     std::cout << "'" << static_cast<char>(input[0]) << "'" << std::endl;
-    if (d < 0 || d > 127 || input == "nan" || input == "nanf" )
-        std::cout << "impossible" << std::endl;
-    else if (!isprint(static_cast<int>(d)))
-            std::cout << "Non displayable" << std::endl;
-    else
-        std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
+//     std::cout << "char : ";
+//     if (input.length() == 1 && !isdigit(d))
+//         std::cout << "'" << static_cast<char>(input[0]) << "'" << std::endl;
+//     if (d < 0 || d > 127 || input == "nan" || input == "nanf" )
+//         std::cout << "impossible" << std::endl;
+//     else if (!isprint(static_cast<int>(d)))
+//             std::cout << "Non displayable" << std::endl;
+//     else
+//         std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
 
-    std::cout << "int : ";
-    // if (input.length() == 1 && !isdigit(d))
-    //     std::cout << static_cast<int>(input[0]) << std::endl;
-    if (d > INT_MAX || d < INT_MIN || input == "nan" || input == "nanf")
-        std::cout << "impossible" << std::endl;
-    else
-        std::cout << static_cast<int>(d) << std::endl;
+//     std::cout << "int : ";
+
+//     if (d > INT_MAX || d < INT_MIN || input == "nan" || input == "nanf")
+//         std::cout << "impossible" << std::endl;
+//     else
+//         std::cout << static_cast<int>(d) << std::endl;
     
-    std::cout << std::fixed << std::setprecision(1);
-    std::cout << "float : ";
-    // if (input.length() == 1 && !isdigit(d))
-    //     std::cout << static_cast<float>(input[0]) << std::endl;
-    // else
-    std::cout << static_cast<float>(d) << "f" << std::endl;
+//     std::cout << std::fixed << std::setprecision(1);
+//     std::cout << "float : ";
+//     std::cout << static_cast<float>(d) << "f" << std::endl;
+//     std::cout << "double : ";
+//     std::cout << static_cast<double>(d) << std::endl;
+// }
 
-    std::cout << "double : ";
-    // if (input.length() == 1 && !isdigit(d))
-    //     std::cout << static_cast<double>(input[0]) << std::endl;
-    // else 
-    std::cout << static_cast<double>(d) << std::endl;
+int isChar(std::string input)
+{
+    const char *str = input.c_str();
+
+    for (int i = 0; str[i]; i++)
+    {
+        if (!isalpha(str[i]))
+            return 0;
+    }
+    return 1;
+}
+
+int isInt(std::string input)
+{
+    const char *str = input.c_str();
+
+    for (int i = 0; str[i]; i++)
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return 0;
+    }
+    return 1;
+}
+int isFloat(std::string input)
+{
+    
+    return 0;
+}
+
+int isDouble(std::string input)
+{
+    int last = input.length();
+    if (input[last] != 'f')
+        return 0;
+    return 1;
+}
+
+void ScalarConverter::convert(std::string input)
+{
+    if (isChar(input))
+        std::cout << "it's a char" << std::endl;
+    else if (isInt(input))
+        std::cout << "it's an int" << std::endl;
+    else if (isFloat(input))
+        std::cout << "it's a float" << std::endl;
+    else if (isDouble(input))
+        std::cout << "it's a double" << std::endl;
+    else
+        std::cout << "it's unknown" << std::endl;
 }
