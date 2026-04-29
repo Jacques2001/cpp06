f1525e2edb155e1e9d4429eb40c3bb9eed7c0515
Jacques2001 <jacques-qiu@hotmail.com>
2026-04-29 17:57:43 +0200

    done

---
+++ ex02/Base.cpp
@@ -0,0 +1,52 @@
+#include "Base.hpp"
+
+Base *generate(void)
+{
+    int nb = 0;
+
+    srand(time(NULL));
+    nb = rand() % 100;
+    if (nb >= 0 && nb <= 33)
+        return new A();
+    else if (nb > 33 && nb <= 66)
+        return new B();
+    else
+        return new C();
+}
+
+void identify(Base *p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "Pointer = A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "Pointer = B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "Pointer = C" << std::endl;
+}
+
+void identify(Base &p)
+{
+    try 
+    {
+        Base &b = dynamic_cast<A&>(p); 
+        (void)b;
+        std::cout << "Reference = A" << std::endl;
+    }
+    catch(std::exception &e) {}
+
+    try 
+    {
+        Base &b = dynamic_cast<B&>(p); 
+        (void)b;
+        std::cout << "Reference = B" << std::endl;
+    }
+    catch(std::exception &e) {} 
+   
+    try 
+    {
+        Base &b = dynamic_cast<C&>(p); 
+        (void)b;
+        std::cout << "Reference = C" << std::endl;
+    }
+    catch(std::exception &e) {}
+}
